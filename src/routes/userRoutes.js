71964c1310ef8ad309e1904a51fe46a86a167e02
thaidev13639 <pnt0964413557@gmail.com>
2024-01-20 18:03:45 +0700

    add config cloudinary, rename config, create api UploadAvatar, add link api to api.text, crearte Verify Token

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { handleLogin, handleSignUp } from "../controller/userController.js";
+import {
+  handleLogin,
+  handleSignUp,
+  handleUploadAvatar,
+} from "../controller/userController.js";
+import { verifyToken } from "../config/jwt.js";
+import uploadCloud from "../config/cloudinary.config.js";
 
 const userRoutes = express.Router();
 
 userRoutes.post("/login", handleLogin);
 userRoutes.post("/signup", handleSignUp);
+userRoutes.post("/upload_avatar", verifyToken, uploadCloud.single("file") , handleUploadAvatar);
 
 export default userRoutes;
