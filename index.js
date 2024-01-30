3931b3aadbea1396ad04581faf175364e78880e9
BAO TRANG <baogtrang@gmail.com>
2024-01-30 12:12:19 -0600

    Bao first commit

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ dotenv.config();
 let portBE = process.env.PORT_BE;
 
 const app = express();
-const port = portBE;
+const port = 8080;
 
 app.use(express.json());
 app.use(express.static("."));
 app.use(cors());
 app.use(rootRoutes);
 
-// app.get("/", (req, res) => {
-//   res.send("Connect BE Success");
-// });
+app.get("/", (req, res) => {
+  res.send("Connect BE Success");
+});
 
 app.listen(port, () => {
   console.log(`Start Working BE With Port : ${port}`);
